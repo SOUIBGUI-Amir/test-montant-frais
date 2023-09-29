ef0c04acd4218e260816b7632fef7ba3a096a251
asoui <amir.souibgui@lmdsio.fr>
2023-09-29 15:26:33 +0200

    Initial commit

---
+++ src/MontantValidator.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Souibgui\TestMontantFrais;
+
+class MontantValidator
+{
+
+    public static function validerMontant($montant)
+    {
+
+
+        return is_numeric($montant) && $montant >= 0.01 && $montant<=200;
+
+ }
+
+}
